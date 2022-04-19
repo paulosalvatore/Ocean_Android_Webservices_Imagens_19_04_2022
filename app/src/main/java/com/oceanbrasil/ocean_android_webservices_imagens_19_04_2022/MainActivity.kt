28feb8f0f47cdea628418fbc17dd9d37ab40ea53
Paulo Salvatore <salvatoregames@gmail.com>
2022-04-19 10:22:30 -0300

    Inicialização do retrofit

--- app/src/main/java/com/oceanbrasil/ocean_android_webservices_imagens_19_04_2022/MainActivity.kt
+++ app/src/main/java/com/oceanbrasil/ocean_android_webservices_imagens_19_04_2022/MainActivity.kt
@@ -2,10 +2,18 @@ package com.oceanbrasil.ocean_android_webservices_imagens_19_04_2022
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Inicialização do retrofit
+        val retrofit = Retrofit.Builder()
+            .baseUrl("https://pokeapi.co/api/v2/")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
     }
 }
