cd959b95f6a7191481c2da2b2bec22e3f849cbfb
Paulo Salvatore <salvatoregames@gmail.com>
2022-04-19 11:05:27 -0300

    Implementação da chamada na API do pokemon

--- app/src/main/java/com/oceanbrasil/ocean_android_webservices_imagens_19_04_2022/MainActivity.kt
+++ app/src/main/java/com/oceanbrasil/ocean_android_webservices_imagens_19_04_2022/MainActivity.kt
@@ -2,6 +2,10 @@ package com.oceanbrasil.ocean_android_webservices_imagens_19_04_2022
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
@@ -15,5 +19,30 @@ class MainActivity : AppCompatActivity() {
             .baseUrl("https://pokeapi.co/api/v2/")
             .addConverterFactory(GsonConverterFactory.create())
             .build()
+
+        // Inicializar o Service
+        val service = retrofit.create(PokemonService::class.java)
+
+        // Fazer a requisição
+        val call = service.listarPokemon()
+
+        call.enqueue(object : Callback<PokemonApiResult> {
+            override fun onResponse(
+                call: Call<PokemonApiResult>,
+                response: Response<PokemonApiResult>
+            ) {
+                if (response.isSuccessful) {
+                    val pokemonApiResult = response.body()
+
+                    pokemonApiResult?.let {
+                        Toast.makeText(this@MainActivity, "Pokemon carregados com sucesso.", Toast.LENGTH_LONG).show()
+                    }
+                }
+            }
+
+            override fun onFailure(call: Call<PokemonApiResult>, t: Throwable) {
+                Toast.makeText(this@MainActivity, "Falha na requisição.", Toast.LENGTH_LONG).show()
+            }
+        })
     }
 }
