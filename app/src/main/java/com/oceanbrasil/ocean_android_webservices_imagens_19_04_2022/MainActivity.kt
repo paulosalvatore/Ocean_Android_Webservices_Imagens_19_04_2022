66846881309b951e6c0b8296ee9f210c87d62620
Paulo Salvatore <salvatoregames@gmail.com>
2022-04-19 09:26:15 -0300

    Initial commit

---
+++ app/src/main/java/com/oceanbrasil/ocean_android_webservices_imagens_19_04_2022/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.oceanbrasil.ocean_android_webservices_imagens_19_04_2022
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
